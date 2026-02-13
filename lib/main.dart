a09461552664e8a8550ada0e5cf72ea1bcef9958
Muhammd Usman <usman4009797@gmail.com>
2026-02-13 06:46:47 +0500

    My local changes before merge

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,6 @@ import 'core/utils/navigation_service.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // // 🔹 Supabase initialize
-  // await Supabase.initialize(
-  //   url: 'https://qgrmjmcrupzyaxamltyq.supabase.co', // tumhara Project URL
-  //   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFncm1qbWNydXB6eWF4YW1sdHlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzODIyMjMsImV4cCI6MjA4NTk1ODIyM30.J_COoSDs1mQUDMzzuPkHLfadr93oCIp3Sim6CAw3XCs',
-  // );
-
   runApp(const MyApp());
 }
 
